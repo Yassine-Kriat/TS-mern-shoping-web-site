4d193601b529f89592d224bc844e81b26b1b8ecb
Yassine Kriat <kriatyassine@gmail.com>
2023-05-04 12:01:39 +0100

    update list product

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProduct } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,19 @@ function App() {
   return (
     <div>
       <header>TS Mcommerce</header>
-      <main></main>
+      <main>
+        {sampleProduct.map((product) => (
+          <li key={product.slug}>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="product-image"
+            />
+            <h2>{product.name}</h2>
+            <p>${product.price}</p>
+          </li>
+        ))}
+      </main>
       <footer>all right reserved</footer>
     </div>
   )
