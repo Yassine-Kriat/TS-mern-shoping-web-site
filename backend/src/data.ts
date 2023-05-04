1993e9dafc71ef703f8aa6dda4ef268e3c26667e
Yassine Kriat <kriatyassine@gmail.com>
2023-05-04 16:44:10 +0100

    create express server

---
+++ backend/src/data.ts
@@ -0,0 +1,48 @@
+import { Product } from './types/Product'
+
+export const sampleProduct: Product[] = [
+  {
+    name: "6 façons d'assortir la table et les chaises de salle à manger avec style",
+    slug: "6 façons d'assortir la table et les chaises de salle à manger avec style",
+    category: 'Salle à manger',
+    image: '../images/p1.jpg',
+    price: 120,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 10,
+    description: 'high quality product',
+  },
+  {
+    name: 'Des assiettes en installation murale',
+    slug: 'Des assiettes en installation murale',
+    category: 'Salle à manger',
+    image: '../images/p2.jpg',
+    price: 100,
+    countInStock: 20,
+    rating: 4.0,
+    numReviews: 10,
+    description: 'high quality product',
+  },
+  {
+    name: 'Tapis 100% Coton Naturel Jute Réversible Modern Living Zone Décor De',
+    slug: 'Tapis 100% Coton Naturel Jute Réversible Modern Living Zone Décor De',
+    category: 'sallon',
+    image: '../images/p3.jpg',
+    price: 220,
+    countInStock: 0,
+    rating: 4.8,
+    numReviews: 17,
+    description: 'high quality product',
+  },
+  {
+    name: 'Tapis Unique en laine, Handmade Vintage Tribal marocain Tissé à la main 110x61Cm',
+    slug: 'Tapis Unique en laine, Handmade Vintage Tribal marocain Tissé à la main 110x61Cm',
+    category: 'cuisine',
+    image: '../images/p4.jpg',
+    price: 78,
+    countInStock: 15,
+    rating: 4.5,
+    numReviews: 14,
+    description: 'high quality product',
+  },
+]
---
+++ backend/src/types/Product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  name: string
+  slug: string
+  image: string
+  category: string
+  price: number
+  countInStock: number
+  description: string
+  rating: number
+  numReviews: number
+}
