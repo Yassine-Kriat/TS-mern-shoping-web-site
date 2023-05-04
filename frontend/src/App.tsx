c1799e132a40200ff7073d31838dc0df2c1cd48e
Yassine Kriat <kriatyassine@gmail.com>
2023-05-04 12:39:59 +0100

    adding react bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { sampleProduct } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <header>TS Mcommerce</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>Mcommerce</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              cart
+            </a>
+            <a href="/signin" className="nav-link ">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        {sampleProduct.map((product) => (
-          <li key={product.slug}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </li>
-        ))}
+        <Container className="mt-3">
+          <Row>
+            {sampleProduct.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>all right reserved</footer>
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
     </div>
   )
 }
